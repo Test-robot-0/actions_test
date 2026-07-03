a6e31cdbb5298c1ab4ab80d191a09a0b3ff04e66
Test-robot-0 <muli.24bcy10254@vitbhopal.ac.in>
2026-07-03 14:50:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,24 @@
 from playwright.sync_api import sync_playwright
 
-print("Starting...")
-
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
     page = browser.new_page()
 
     url = "https://leetcode.com/problems/asteroid-collision/description/"
-    page.goto(url)
-    print("URL:", page.url)
-    print("Title:", page.title())
 
-    html = page.content()
+    page.goto(url, wait_until="domcontentloaded", timeout=60000)
 
-    print(html[:500])
-    
-    page.wait_for_timeout(10000)
+    # Give Cloudflare some time if it's going to automatically verify.
+    page.wait_for_timeout(30000)
 
     print("URL:", page.url)
     print("Title:", page.title())
 
-    html = page.content()
+    page.screenshot(path="page.png", full_page=True)
 
-    print("Length:", len(html))
+    html = page.content()
 
     with open("page.html", "w", encoding="utf-8") as f:
         f.write(html)
 
     browser.close()
-
-print("Done")
